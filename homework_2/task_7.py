71afcf955a44582cbea3cf7ae7149ee97f392a15
Maksim <homeice@mail.ru>
2023-12-20 04:18:55 +0300

    task 7 complete

---
+++ homework_2/task_7.py
@@ -0,0 +1,29 @@
+'''
+Создать страницу, на которой будет форма для ввода числа
+и кнопка "Отправить"
+При нажатии на кнопку будет произведено
+перенаправление на страницу с результатом, где будет
+выведено введенное число и его квадрат.
+'''
+
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def index():
+    return render_template('form_square.html')
+
+@app.post('/')
+def index_post():
+    num = float(request.form.get('num'))
+    square = num**2
+
+    return render_template('form_square.html', num=num, square=square)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
